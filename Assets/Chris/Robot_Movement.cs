1b56a9c7df5acd67e2814744014d7b737a09fe3a
mendoza1 <csmendoza@ucdavis.edu>
2018-01-26 23:25:13 -0800

    Chris folder added

---
+++ Assets/Chris/Robot_Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot_Movement : MonoBehaviour {
+
+	Rigidbody2D rb; //to move the robot(s)
+
+	// Use this for initialization
+	void Start () {
+		
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		rb.velocity = 
+	}
+}
