5167df44c0a8301d156f1ed8a29de94e21963c03
Thomas Tran <thomastran878@gmail.com>
2018-01-27 16:48:58 -0800

    Fixing duplicate scripts

--- Assets/Chris/Scripts/Robot_Movement.cs
+++ Assets/Chris/Scripts/Robot_Movement.cs
@@ -5,33 +5,38 @@ using UnityEngine;
 public class Robot_Movement : MonoBehaviour {
 
 	Rigidbody2D rb; //to move the robot(s)
-	private int maxSpeed = 10;
+    [SerializeField]
+	private float maxSpeed;
 
-	public float playerFrequency; //the frequency the player can change
-	public float robotFrequency; //this robot's frequency
+	//public float playerFrequency; //the frequency the player can change
+	//public float robotFrequency; //this robot's frequency
 
-	float GetXVelocityRelativeToFrequency() {
-
-		float diffInFrequecies = Mathf.Abs (playerFrequency - robotFrequency);
-		float speedFactor = maxSpeed - diffInFrequecies;
-		float factorOfMaxSpeed = maxSpeed / 3.0f;
-		//half as slow, felt too fast at max speed
-		return (Input.GetAxisRaw ("Horizontal") * speedFactor) / factorOfMaxSpeed;
-	}
+    private FrequencyReceiver freqReceive;
 
 	// Use this for initialization
 	void Start () {
 		
 		rb = GetComponent<Rigidbody2D> ();
+        freqReceive = gameObject.GetComponent<FrequencyReceiver>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
 	void FixedUpdate() {
 		
 		rb.velocity = new Vector2 (GetXVelocityRelativeToFrequency(), rb.velocity.y);
 	}
+
+    float GetXVelocityRelativeToFrequency()
+    {
+
+        //float diffInFrequecies = Mathf.Abs (playerFrequency - robotFrequency);
+        //float speedFactor = maxSpeed - diffInFrequecies;
+        float speedFactor = maxSpeed - freqReceive.getFrequencyDiff();
+        float factorOfMaxSpeed = maxSpeed / 3.0f;
+        //half as slow, felt too fast at max speed
+        return (Input.GetAxisRaw("Horizontal") * speedFactor) / factorOfMaxSpeed;
+    }
 }
