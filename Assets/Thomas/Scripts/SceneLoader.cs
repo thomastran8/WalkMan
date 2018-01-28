20c1e3871376eb4f9dd42b273be1758fceca3faa
Thomas Tran <thomastran878@gmail.com>
2018-01-27 21:21:50 -0800

    Edited Menu and added reset button

--- Assets/Thomas/Scripts/SceneLoader.cs
+++ Assets/Thomas/Scripts/SceneLoader.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour {
+    private int currentScene;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,12 +18,16 @@ public class SceneLoader : MonoBehaviour {
 
     public void loadNextScene()
     {
-        int currentScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentScene+1);
     }
 
     public void exitGame()
     {
         Application.Quit();
+    }
+
+    public void resetGame()
+    {
+        SceneManager.LoadScene(currentScene);
     }
 }
