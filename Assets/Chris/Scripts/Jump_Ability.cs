515ffb69985a4f4afbde685e82d2911d38cd5fc2
mendoza1 <csmendoza@ucdavis.edu>
2018-01-27 22:16:40 -0800

    Control panel, jumping, moving platform

---
+++ Assets/Chris/Scripts/Jump_Ability.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump_Ability : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate() {
+
+	}
+
+	void OnTriggerStay2D(Collider2D obj) {
+
+		//the player pressed the space bar
+		if (Input.GetKeyDown ("space")) {
+
+			//they are jumping off another robot, or the ground
+			if(obj.gameObject.tag == "Inmate" || obj.gameObject.tag == "Ground") {
+		 
+				rb.AddForce (new Vector2 (0, 5), ForceMode2D.Impulse);
+			}
+		}
+	}
+}
