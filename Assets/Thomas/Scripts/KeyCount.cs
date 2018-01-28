d758ffbcfa9c6fc88e191f4662c3cad2564b13c6
Thomas Tran <thomastran878@gmail.com>
2018-01-27 22:35:29 -0800

    Added key pickup

---
+++ Assets/Thomas/Scripts/KeyCount.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCount : MonoBehaviour {
+    private static int numKeys = 0;
+    private static int keysInLevel;
+    private static SceneLoader sceneLoad;
+
+    // Use this for initialization
+    void Start () {
+        keysInLevel = GameObject.FindGameObjectsWithTag("Key").Length;
+        sceneLoad = GameObject.FindGameObjectWithTag("SceneLoader").GetComponent<SceneLoader>();
+        numKeys = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public int getNumKeys()
+    {
+        return numKeys;
+    }
+
+    public static void incrementKeyCount()
+    {
+        numKeys++;
+        if (numKeys >= keysInLevel)
+        {
+            sceneLoad.loadNextScene();
+        }
+    }
+}
---
+++ Assets/Thomas/Scripts/KeyPick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPick : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" || collision.tag == "Inmate")
+        {
+            KeyCount.incrementKeyCount();
+            Destroy(gameObject);
+        }
+    }
+}
