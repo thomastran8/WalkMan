d445f4d5376404f44308702155102cb7ef92f99e
Thomas Tran <thomastran878@gmail.com>
2018-01-27 23:21:16 -0800

    Adding some sprites

---
+++ Assets/Thomas/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    [SerializeField]
+    private float patrolDistance;
+    Rigidbody2D rb;
+    private float startPoint;
+    private float endpoint;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        startPoint = transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+
+    }
+}
