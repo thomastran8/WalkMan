49bf472efd61fa97cdd7fb59ea4a204e3bb99dd1
mendoza1 <csmendoza@ucdavis.edu>
2018-01-27 18:09:07 -0800

    Lightning displayed

--- Assets/Thomas/Scripts/FrequencyReceiver.cs
+++ Assets/Thomas/Scripts/FrequencyReceiver.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FrequencyReceiver : MonoBehaviour {
+	//shown when player's frequency is close to this robot's
+	public GameObject lightningBolt; 
+
     private float playerRefSpeed = 0;
     private FrequencyController fc;
     [Range(0.0f, 10.0f)]
     [SerializeField]
     private float frequencyChannel;
     private bool randomizeStartingFreq;
+
     private GameObject playerGO;
     private float frequencyDiff;
+
+	private float lightningFreqRange = 0.25f;
+
 
     // Use this for initialization
     void Start () {
@@ -22,11 +29,17 @@ public class FrequencyReceiver : MonoBehaviour {
 	void Update () {
         float receivingFrequency = fc.getCurrentFreq();
         frequencyDiff = Mathf.Abs(receivingFrequency - frequencyChannel);
+
+		if (frequencyDiff <= lightningFreqRange) //display lightning bolt
+			lightningBolt.SetActive(true);
+		else
+			lightningBolt.SetActive(false);
 	}
 
     // The amount of frequency difference between the player's frequency and this robot
     public float getFrequencyDiff()
     {
         return frequencyDiff;
+
     }
 }
